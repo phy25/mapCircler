70b5b274e2cd0013a850de9d955146fc1597db75
Panzj <stpanzj@189.cn>
2013-07-28 13:57:38 +0800

    分离 JavaScript，更换 JS 库。

--- mapCircler.js
+++ mapCircler.js
@@ -2,14 +2,14 @@
 function PolygonCenter (poly) {
 	var x,y;
 
-    var vertices = poly.getPath();
+	var vertices = poly.getPath();
 
-      // Iterate over the vertices.
-    for (var i=0, x=0, y=0; i < vertices.length; i++) {
-    	var xy = vertices.getAt(i);
-        x = x + xy.lat();
-        y = y + xy.lng();
-    }		
+	// Iterate over the vertices.
+	for (var i=0, x=0, y=0; i < vertices.length; i++) {
+		var xy = vertices.getAt(i);
+		x = x + xy.lat();
+		y = y + xy.lng();
+	}
 	x = x / vertices.length;
 	y = y / vertices.length;
 	return new google.maps.LatLng(x, y);
@@ -139,7 +139,7 @@ function addSubmit(regionOrder,poly){
 	jsonObj.ID++;
 	poly.zIndex = jsonObj.ID;
 
-	jsonObj.regions[regionOrder].elements.push({"mode":0,"info":$('#item_meta').val(),"name":$('#item_name').val(), "ID":jsonObj.ID});
+	jsonObj.regions[regionOrder].elements.push({"mode":0,"info":document.getElementById('item_meta').value,"name":document.getElementById('item_name').value, "ID":jsonObj.ID});
 		
 	var path = poly.getPath();
 
@@ -165,34 +165,34 @@ function addSubmit(regionOrder,poly){
 function checkChildID(jsonObj, oldID, regionOrder, elementIndex,modify){
 	var indexObj = jsonObj.regions[regionOrder].elements[elementIndex];
 	var newID = indexObj.ID;
-    if (modify) {
-	for (i=0; i<jsonObj.regions.length; i++){
-		if (jsonObj.regions[i].ID == oldID){
-			jsonObj.regions[i].ID = newID;
-			jsonObj.regions[i].paths = [];
-			for (j=0; j<indexObj.paths.length; j++){
-				jsonObj.regions[i].paths.push(indexObj.paths[j]);
+	if (modify) {
+		for (i=0; i<jsonObj.regions.length; i++){
+			if (jsonObj.regions[i].ID == oldID){
+				jsonObj.regions[i].ID = newID;
+				jsonObj.regions[i].paths = [];
+				for (j=0; j<indexObj.paths.length; j++){
+					jsonObj.regions[i].paths.push(indexObj.paths[j]);
+				}
+				break;
 			}
-			break;
+		}
+	} else {
+		var found = false, deleteRegion = false;
+		for (i=0; i<jsonObj.regions.length; i++){
+			if (jsonObj.regions[i].ID == oldID){
+				if (found) { 
+					found = true;
+					if (confirm("删除下属单元，不保留在上一级区域中")) deleteRegion = true;
+				}
+				if (deleteRegion) {
+					jsonObj.regions.splice(i,i+1);
+					i--;
+				} else {
+					
+				}
+			}
 		}
 	}
-    } else {
-	var found = false, deleteRegion = false;
-	for (i=0; i<jsonObj.regions.length; i++){
-		if (jsonObj.regions[i].ID == oldID){
-			if (found) { 
-				found = true;
-				if (confirm("删除下属单元，不保留在上一级区域中")) deleteRegion = true;
-			}
-    			if (deleteRegion) {
-				jsonObj.regions.splice(i,i+1);
-				i--;
-    			} else {
-				
-    			}
-		}
-	}
-    }
 }
 
 // 画指定区域，mode = true: 建立新的区域; mode = false: 重画该区域
@@ -208,7 +208,7 @@ function createRegion(map, jsonObj, regionOrder, mode){
 		drawBlockJson(map, jsonObj, regionOrder, j, mode);
 	};
 
-// 画外框
+	// 画外框
 	var indexObj = jsonObj.regions[regionOrder];
 	var path=[];
 	for (var i =0; i < indexObj.paths.length; i++) {
@@ -249,7 +249,7 @@ function readFile(){
 		// 如果不支持 HTML5 读取文件
 		var text = prompt('请将文件内容粘贴到这里并按 Enter。','');
 		if(text){
-			jsonObj = $.evalJSON(jsontext);
+			jsonObj = JSON.parse(jsontext);
 			deleteCurrentRegion(map);			
 			currentRegionOrder = getRegionOrder(jsonObj, jsonObj.currentID);
 			createRegion(map, jsonObj, currentRegionOrder, true);
@@ -266,7 +266,7 @@ function readFile(){
 			// 读取文件完成时执行 onLoad 事件，先设置事件
 			reader.onload = function(e){
 				if(e.target.readyState == 2){
-					jsonObj = $.evalJSON(e.target.result);
+					jsonObj = JSON.parse(e.target.result);
 					deleteCurrentRegion(map);
 					currentRegionOrder = getRegionOrder(jsonObj, jsonObj.currentID);
 					createRegion(map, jsonObj, currentRegionOrder, true);
@@ -277,23 +277,14 @@ function readFile(){
 
 			reader.readAsText(file);
 		};
-		$f.change(change).appendTo('body').click();
+		$f.change(change).click();
 	}
 
 }
 
 // 导出文件部分
 function saveFile(){ // text, filename	
-	if($.browser.chrome){
-		var filename = prompt('请输入文件名');
-		var $h = $('<div style="position:absolute;z-index:10;top:0;left:0;width:50px;height:30px;"></div>'), $a = $('<a />').attr('href', 'data:text/octet-stream,' + encodeURIComponent($.toJSON(jsonObj))).attr('download', filename+'.mcf').text('点击').click(function(){$h.remove();});
-		$h.append($a).appendTo('body');
-		//download 属性定义下载的文件名
-	}else{
-		var $h = $('<div style="position:absolute;z-index:10;top:0;left:0;width:50px;height:30px;"><textarea style="width:40px;height:30px;"></textarea> <a href="javascript:void(0)">OK</a></div>');
-		$h.find('a').click(function(){$h.remove();return false;});		
-		$h.appendTo('body').find('textarea').text($.toJSON(jsonObj)).focus();
-	}
+	saveAs(new Blob([JSON.stringify(jsonObj)], {type: "application/json;charset=utf-8"}), prompt('请输入文件名')+".mcf");
 }
 
 MapOverlay.prototype = new google.maps.OverlayView();
@@ -365,17 +356,17 @@ function initialize() {
 		drawingManager.setDrawingMode(null);
 		poly.setEditable(true);
 
-   		$('#item_name').val('');
-    	$('#item_meta').val('');
+		document.getElementById('item_name').value = '';
+		document.getElementById('item_meta').value = '';
 		document.getElementById("info").style.display="";
 	
 		google.maps.event.addDomListenerOnce(document.getElementById('item_acceptBtn'), 'click', function (){
-			if(!$('#item_name').val()){
+			if(!document.getElementById('item_name').value){
 				alert('请输入名称。');
 			return false;
 			};
 			addSubmit(currentRegionOrder,poly);
-	    	document.getElementById("info").style.display="none";
+			document.getElementById("info").style.display="none";
 			$("#map_shantou").contextMenu(true);
 			return false;
 		});
@@ -385,8 +376,8 @@ function initialize() {
 			poly.setMap(null);
 			poly = null;
 			// 关闭对话窗口
-	    	document.getElementById("info").style.display="none";
-		$("#map_shantou").contextMenu(true);
+			document.getElementById("info").style.display="none";
+			$("#map_shantou").contextMenu(true);
 			return false;
 		});
 
@@ -395,13 +386,13 @@ function initialize() {
 			poly.setMap(null);
 			poly = null;
 			// 关闭对话窗口
-	    	document.getElementById("info").style.display="none";
+			document.getElementById("info").style.display="none";
 		$("#map_shantou").contextMenu(true);
 			return false;
 		});
 
 
-		$('#item_name').focus();
+		document.getElementById('item_name').focus();
 	});
 
 // create interactive window
@@ -410,95 +401,94 @@ function initialize() {
 // Construct the layer
 	createRegion(map, jsonObj, currentRegionOrder, true);
 
-// 处理菜单
-    $.contextMenu({ 
-        selector: '#map_shantou',
-        build: function($trigger, e){
-	//console.log(e);
-	return {
-	callback: function(key, options) {
-		var func = {
-                    'load': function(){
-                        $("#map_shantou").contextMenu("hide");
-			readFile();
-        		// return false;
-                    },
-                    'save': function(){
-			$("#map_shantou").contextMenu("hide");
-        		saveFile();
-			// return false;
-                    },
-                    'enter':function(){
-                        $("#map_shantou").contextMenu("hide");
-			if (e.eventName != 'polygon') return;			
-			var poly = e.originalThis;
-			var newRegionOrder = getRegionOrder(jsonObj, poly.zIndex);
-			if (newRegionOrder == null) {
-				if (confirm("没有下一层，增加吗？")==false)return;				
-				newRegionOrder = createNextRegion(poly);
+	// 处理菜单
+	$.contextMenu({ 
+		selector: '#map_shantou',
+		build: function($trigger, e){
+		//console.log(e);
+		return {
+		callback: function(key, options) {
+			var func = {
+				'load': function(){
+					$("#map_shantou").contextMenu("hide");
+					readFile();
+					// return false;
+				},
+				'save': function(){
+					$("#map_shantou").contextMenu("hide");
+					saveFile();
+					// return false;
+				},
+				'enter':function(){
+					$("#map_shantou").contextMenu("hide");
+					if (e.eventName != 'polygon') return;			
+					var poly = e.originalThis;
+					var newRegionOrder = getRegionOrder(jsonObj, poly.zIndex);
+					if (newRegionOrder == null) {
+						if (confirm("没有下一层，增加吗？")==false) return;				
+						newRegionOrder = createNextRegion(poly);
+					}
+					deleteCurrentRegion(map);
+					currentRegionOrder = newRegionOrder;
+					createRegion(map, jsonObj, currentRegionOrder, true);
+				},
+				'return':function(){
+					$("#map_shantou").contextMenu("hide");
+					var newlayer = jsonObj.regions[currentRegionOrder].layer;
+					var ID = jsonObj.regions[currentRegionOrder].ID;
+					var newRegionOrder = null;
+
+					if (newlayer == 0) return;
+					for (j=0,newlayer--;  j < jsonObj.regions.length; j++){
+						if (jsonObj.regions[j].layer != newlayer) continue;
+						for (i=0; i < jsonObj.regions[j].elements.length; i++){
+							if (jsonObj.regions[j].elements[i].ID == ID) {
+								newRegionOrder = j;
+								break;
+							}
+						}
+					}
+
+					if (newRegionOrder == null) return;
+					deleteCurrentRegion(map);
+					currentRegionOrder = newRegionOrder;
+					createRegion(map, jsonObj, currentRegionOrder, true);
+					return false;
+				},
+				'edit':function(){
+					$("#map_shantou").contextMenu("hide");
+					
+					if (e.eventName == 'polygon') {
+						$("#map_shantou").contextMenu(false);
+						editBlock.call(e.originalThis);
+					}
+				},
+				'new':function(){
+					$("#map_shantou").contextMenu("hide");
+					$("#map_shantou").contextMenu(false);
+					drawingManager.setDrawingMode("polygon");
+				},
+				'frame':function(){
+					reurn;
+				}
+			};
+			return func[key] && func[key]();//if(func[key]){func();}
+		},
+		items: {
+			"load": {name: "打开文件"},
+				"save": {name: "保存文件"},
+				"sep1": "---------",
+				"enter": {name: "进入下一层"},
+				"return": {name: "返回上一层"},
+				"sep2": "---------",
+				"edit": {name: "编辑本单元", icon: "edit"},
+				"new": {name: "新建单元", icon: "add"},
+				"frame":{name:"编辑边界"}
 			}
-			deleteCurrentRegion(map);
-			currentRegionOrder = newRegionOrder;
-			createRegion(map, jsonObj, currentRegionOrder, true);
-
-                    },
-                    'return':function(){
-			$("#map_shantou").contextMenu("hide");
-			var newlayer = jsonObj.regions[currentRegionOrder].layer;
-    			var ID = jsonObj.regions[currentRegionOrder].ID;
-    			var newRegionOrder = null;
-
-    			if (newlayer == 0) return;
-    			for (j=0,newlayer--;  j < jsonObj.regions.length; j++){
-    				if (jsonObj.regions[j].layer != newlayer) continue;
-    				for (i=0; i < jsonObj.regions[j].elements.length; i++){
-    					if (jsonObj.regions[j].elements[i].ID == ID) {
-    						newRegionOrder = j;
-    						break;
-    					}
-    				}
-    			}
- 
- 			if (newRegionOrder == null) return;
-			deleteCurrentRegion(map);
-			currentRegionOrder = newRegionOrder;
-			createRegion(map, jsonObj, currentRegionOrder, true);
-        		return false;
-			},
-                    'edit':function(){
-			$("#map_shantou").contextMenu("hide");
-			
-			if (e.eventName == 'polygon') {
-				$("#map_shantou").contextMenu(false);
-				editBlock.call(e.originalThis);
-                        }
-                    },
-                    'new':function(){
-			$("#map_shantou").contextMenu("hide");
-			$("#map_shantou").contextMenu(false);
-			drawingManager.setDrawingMode("polygon");
-                    },
-                    'frame':function(){
-                    	reurn;
-                    }
-                };
-            return func[key] && func[key]();//if(func[key]){func();}
-        },
-        items: {
-	    "load": {name: "打开文件"},
-            "save": {name: "保存文件"},
-            "sep1": "---------",
-            "enter": {name: "进入下一层"},
-            "return": {name: "返回上一层"},
-            "sep2": "---------",
-            "edit": {name: "编辑本单元", icon: "edit"},
-            "new": {name: "新建单元", icon: "add"},
-            "frame":{name:"编辑边界"}
-        }
-	}}
-    });
-    
-    google.maps.event.addListener(map, "rightclick", rightClickHandler);
+		}}
+	});
+	
+	google.maps.event.addListener(map, "rightclick", rightClickHandler);
 
 	mapOverlay = new MapOverlay(map);
 }
@@ -516,11 +506,11 @@ function rightClickHandlerPoly(e){
 	}else{
 		var p = mapOverlay.getProjection().fromLatLngToContainerPixel(e.latLng);
 
-	    	ev.pageX = p.x;
-	    	ev.pageY = p.y;
+			ev.pageX = p.x;
+			ev.pageY = p.y;
 		if(!ev.pageX) window.console ? console.error('Cursor position not found!') : alert('Cursor position not found!');
 	}
-	    // contextMenuEvent = e;
+	// contextMenuEvent = e;
 
 	ev.originalThis = this;
 	ev.latLng = e.latLng;
@@ -530,7 +520,7 @@ function rightClickHandlerPoly(e){
 }
 // 右键处理程序，对应在其他区域上点击
 function rightClickHandler(e){
-    	// console.log(e);
+	// console.log(e);
 	var ev = new jQuery.Event('contextmenu');
 	
 	if(e.pixel){
@@ -542,11 +532,11 @@ function rightClickHandler(e){
 	}else{
 		var p = mapOverlay.getProjection().fromLatLngToContainerPixel(e.latLng);
 
-	    	ev.pageX = p.x;
-	    	ev.pageY = p.y;
+		ev.pageX = p.x;
+		ev.pageY = p.y;
 		if(!ev.pageX) window.console ? console.error('Cursor position not found!') : alert('Cursor position not found!');
 	}
-	    // contextMenuEvent = e;
+	// contextMenuEvent = e;
 
 	ev.originalThis = this;
 	ev.latLng = e.latLng;	
@@ -578,14 +568,13 @@ function editBlock(event) {
 	var elementIndex = getCurrentRegionElenemtzIndex(jsonObj,currentRegionOrder,poly);
 
 	poly.setEditable(true);
-
-    
-    $('#item_name').val(jsonObj.regions[currentRegionOrder].elements[elementIndex].name);
-    $('#item_meta').val(jsonObj.regions[currentRegionOrder].elements[elementIndex].info);
+	
+	document.getElementById('item_name').value = jsonObj.regions[currentRegionOrder].elements[elementIndex].name;
+	document.getElementById('item_meta').value = jsonObj.regions[currentRegionOrder].elements[elementIndex].info;
 	$("#info").show();
 	
 	google.maps.event.addDomListenerOnce(document.getElementById('item_acceptBtn'), 'click', function (){
-		if(!$('#item_name').val()){
+		if(!document.getElementById('item_name').value){
 			alert('请输入名称。');
 			return false;
 		};
@@ -598,8 +587,8 @@ function editBlock(event) {
 		elementIndex = jsonObj.regions[currentRegionOrder].elements.length -1;
 		checkChildID(jsonObj, oldID, currentRegionOrder, elementIndex, true);
 
-	    document.getElementById("info").style.display="none";
-	    $("#map_shantou").contextMenu(true);
+		document.getElementById("info").style.display="none";
+		$("#map_shantou").contextMenu(true);
 		return false;
 	});
 	// 删除改变的图，重画原图。
@@ -618,11 +607,11 @@ function editBlock(event) {
 		var oldID = jsonObj.regions[currentRegionOrder].elements[elementIndex].ID;
 		checkChildID(jsonObj, oldID, currentRegionOrder, elementIndex, false);//BUG***
 		deleBlockJson(map, jsonObj, currentRegionOrder,elementIndex);
-	    document.getElementById("info").style.display="none";
+		document.getElementById("info").style.display="none";
 		$("#map_shantou").contextMenu(true);
 		return false;
 	});
 
 
-	$('#item_name').focus();
+	document.getElementById('item_name').focus();
 }
